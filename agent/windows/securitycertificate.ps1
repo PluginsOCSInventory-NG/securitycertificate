dc2a3164b4c451b26a327592232370884368bd84
Charlene Auger <charlene.auger@factorfx.com>
2018-04-12 14:25:16 +0200

    Replace name by autority if name empty

--- agent/windows/securitycertificate.ps1
+++ agent/windows/securitycertificate.ps1
@@ -5,7 +5,11 @@ $store = Get-ChildItem -Recurse cert: | select *
 foreach ($store in $store){
 	if($store.FriendlyName -ne $null){
 		$xml += "<SECURITYCERTIFICATE>`n"
-		$xml += "<NAME>"+$store.FriendlyName+"</NAME>`n"
+		if($store.FriendlyName.TrimEnd() -eq ""){
+			$xml += "<NAME>"+$store.Issuer+"</NAME>`n"
+		}else{
+			$xml += "<NAME>"+$store.FriendlyName+"</NAME>`n"
+		}
 		$xml += "<AUTORITY>"+$store.Issuer+"</AUTORITY>`n"
 		$xml += "<DATESTART>"+$store.NotBefore+"</DATESTART>`n"
 		$xml += "<EXPIRATION>"+$store.NotAfter+"</EXPIRATION>`n"
